7071eac6d05b6042a5dbeb8eb6c0521a34d2021d
dantheta <dan.abakah@gmail.com>
2013-07-15 09:08:13 +0000

    modified web.js to read index.html from disk

--- web.js
+++ web.js
@@ -1,9 +1,14 @@
 var express = require('express');
+var fs = require('fs');
+var outfile = "index.html";
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  var htmlContent = fs.readFileSync(outfile);
+  var out = htmlContent.toString();
+  // response.end();
+  response.send(out);
 });
 
 var port = process.env.PORT || 5000;
