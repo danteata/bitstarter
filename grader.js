c312925c836c42d1c9de3e859c0c1bd86295b8f5
dantheta <dan.abakah@gmail.com>
2013-07-15 23:37:12 +0000

    modified grader.js for page urls and extended elements to check

--- grader.js
+++ grader.js
@@ -24,8 +24,23 @@ References:
 var fs = require('fs');
 var program = require('commander');
 var cheerio = require('cheerio');
+var rest = require('restler');
 var HTMLFILE_DEFAULT = "index.html";
+var HTMLURL_DEFAULT = "http://lit-earth-1204.herokuapp.com";
 var CHECKSFILE_DEFAULT = "checks.json";
+
+var assertUrlExists = function(inUrl) {
+    var instr = inUrl.toString();
+    // rest.get(instr).on('complete', function(data) {
+    //   if (data instanceof Error) {
+    //     console.log("had an error processing %s. Exiting...", instr);
+    //     process.exit(1); // http://nodejs.org/api/process.html#process_process_exit_code
+    //   }else {
+    //     return instr;
+    //   }
+    // });
+    return instr;
+};
 
 var assertFileExists = function(infile) {
     var instr = infile.toString();
@@ -39,6 +54,13 @@ var assertFileExists = function(infile) {
 var cheerioHtmlFile = function(htmlfile) {
     return cheerio.load(fs.readFileSync(htmlfile));
 };
+
+var restlerHtmlURL = function(htmlUrl) {
+  rest.get(htmlUrl).on('complete', function(data) {
+    console.log("html page data is :: ", data);
+    return cheerio.load(data);
+  });
+}
 
 var loadChecks = function(checksfile) {
     return JSON.parse(fs.readFileSync(checksfile));
@@ -55,6 +77,20 @@ var checkHtmlFile = function(htmlfile, checksfile) {
     return out;
 };
 
+var checkHtmlPage = function(cheerioHtml, checksfile) {
+  rest.get(cheerioHtml).on('complete', function(data) {
+    $ = cheerio.load(data);
+    var checks = loadChecks(checksfile).sort();
+    var out = {};
+    for(var ii in checks) {
+        var present = $(checks[ii]).length > 0;
+        out[checks[ii]] = present;
+    }
+    var outJson = JSON.stringify(out, null, 4);
+    console.log(outJson);
+  });
+};
+
 var clone = function(fn) {
     // Workaround for commander.js issue.
     // http://stackoverflow.com/a/6772648
@@ -64,11 +100,19 @@ var clone = function(fn) {
 if(require.main == module) {
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
-        .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+        .option('-f, --file <html_file>', 'Path to index.html')
+        .option('-u, --url <html_url>', 'Path to page url')
         .parse(process.argv);
-    var checkJson = checkHtmlFile(program.file, program.checks);
-    var outJson = JSON.stringify(checkJson, null, 4);
-    console.log(outJson);
+    var checkJson;
+    if (!program.file) {
+      console.log("using page url");
+      checkJson = checkHtmlPage(program.url, program.checks);
+    }else {
+      console.log("using file path");
+      checkJson = checkHtmlFile(program.file, program.checks);
+      var outJson = JSON.stringify(checkJson, null, 4);
+      console.log(outJson);
+    }
 } else {
     exports.checkHtmlFile = checkHtmlFile;
 }
